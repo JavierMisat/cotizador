15ee2cbc24d0d03db87b5405adf38e3ba5f74dd6
Javier Misat <rjmisal@misena.edu.co>
2018-12-06 11:23:17 -0500

    Segundo commit cotizador seguros app

---
+++ src/components/App.js
@@ -0,0 +1,19 @@
+import React, {Component} from 'react';
+import Header from "./Header";
+import Formulario from "./Formulario";
+
+class App extends Component {
+    render() {
+        return (
+            <div className="contenedor">
+                <Header
+                    titulo='Cotizador de Seguro de Auto'/>
+                <div className="contenedor-formulario">
+                    <Formulario/>
+                </div>
+            </div>
+        );
+    }
+}
+
+export default App;
