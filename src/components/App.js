f07d545cf631fbe5591f4dbead252f31114c392f
Javier Misat <rjmisal@misena.edu.co>
2018-12-07 20:00:15 -0500

    Integrando funciones pequeñas en Helper.js recomendado para no cargar componentes REACT.JS

--- src/components/App.js
+++ src/components/App.js
@@ -1,11 +1,31 @@
 import React, {Component} from 'react';
 import Header from "./Header";
 import Formulario from "./Formulario";
+import {calcularMarca, obtenerDiferenciaAnio, obtenerPlan} from "../helper";
+
 
 class App extends Component {
 
     corizarSeguro = (datos) => {
-        console.log(datos);
+        const {marca, plan, year} = datos;
+
+        //Agregar una base de 2000 pedido por la empresa
+        let resultado = 2000;
+
+        //Obtener la diferencia de años
+        const diferencia = obtenerDiferenciaAnio(year);
+
+        //por cada año restar el 3% del seguro
+        resultado -= ((diferencia * 3) * resultado) / 100;
+
+        //Americano 15% asiatico 5% y europeo 30% de incremento al valor actual
+        resultado = calcularMarca(marca) * resultado;
+
+        //el plan del auto, el básico aumenta el valor 20% y el completo el 50%
+        let incrementoPlan = obtenerPlan(plan);
+
+        //Dependiendo del plan incrementa
+        resultado =  parseFloat(incrementoPlan * resultado).toFixed(2);
     };
 
     render() {
