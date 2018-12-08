035d3c0766590dee2307d4530fcfd0b77b2f460b
Javier Misat <rjmisal@misena.edu.co>
2018-12-08 01:00:23 -0500

    Updates

---
+++ precache-manifest.2f968c5adf68d3d3c92d1213d03c4b12.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "e7f96ab5e68aead5e091",
+    "url": "/cotizador/static/js/runtime~main.e7f96ab5.js"
+  },
+  {
+    "revision": "78f4fbc00886827f04ed",
+    "url": "/cotizador/static/js/main.78f4fbc0.chunk.js"
+  },
+  {
+    "revision": "c1554583cbe2359a2eae",
+    "url": "/cotizador/static/js/1.c1554583.chunk.js"
+  },
+  {
+    "revision": "78f4fbc00886827f04ed",
+    "url": "/cotizador/static/css/main.cf6d2a0a.chunk.css"
+  },
+  {
+    "revision": "13befe2a665a7266a86da227fcbb6ac9",
+    "url": "/cotizador/index.html"
+  }
+];
